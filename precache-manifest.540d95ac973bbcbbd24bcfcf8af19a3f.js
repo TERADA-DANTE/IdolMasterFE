62e4a574340bc034184b421fca35b094ec1d5020
TERADA-MIYA <terada.syun.kim@gmail.com>
2020-01-22 23:31:43 +0900

    Updates

---
+++ precache-manifest.540d95ac973bbcbbd24bcfcf8af19a3f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a43db280f14a0127fb17e6fddddf47ce",
+    "url": "/idolMaster/index.html"
+  },
+  {
+    "revision": "b970b571636670c31747",
+    "url": "/idolMaster/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "12313b0051745445decb",
+    "url": "/idolMaster/static/css/main.4c6a70de.chunk.css"
+  },
+  {
+    "revision": "b970b571636670c31747",
+    "url": "/idolMaster/static/js/2.b7c15033.chunk.js"
+  },
+  {
+    "revision": "4d53bba648496e5052d796c81ac75518",
+    "url": "/idolMaster/static/js/2.b7c15033.chunk.js.LICENSE"
+  },
+  {
+    "revision": "12313b0051745445decb",
+    "url": "/idolMaster/static/js/main.235339cb.chunk.js"
+  },
+  {
+    "revision": "37dbc2fc3bce899a6b20",
+    "url": "/idolMaster/static/js/runtime-main.df29729a.js"
+  },
+  {
+    "revision": "c9a1728db4a67a936a2c67c3a7e5a99f",
+    "url": "/idolMaster/static/media/verified.c9a1728d.svg"
+  }
+]);
