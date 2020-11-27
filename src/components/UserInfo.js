84bb183171909348d063a51626c7fb5ffbaef4a8
TERADA-DANTE <terada.dante@gmail.com>
2020-11-27 15:51:06 +0900

    Update svg attr

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '../assets/css/UserInfo.css'
-import { ReactComponent as VerifiedLogo } from '../assets/icons/verified.svg'
+import { VerifiedLogo } from '../assets/icons/verified.svg'
 
 function UserInfo(props) {
     return (
@@ -13,7 +13,7 @@ function UserInfo(props) {
                     </a>
                     <div />
                     <div className='profileDesc'>
-                        <span><h3 className='userName'>{props.profileDesc.name} {props.profileDesc.isVerified ? <VerifiedLogo width='25' height='25' /> : null}</h3></span>
+                        <span><h3 className='userName'>{props.profileDesc.name} {props.profileDesc.isVerified ? <img src={VerifiedLogo} width='25' height='25' /> : null}</h3></span>
                         <h4 style={{ color: '#6b6b6b' }}>@{props.profileDesc.screen_name}</h4>
                         <span> {props.profileDesc.description}</span><br />
                         <span className='font-weight-bold'>{props.profileDesc.following}</span> <span className='text-secondary'>Following</span><br />
